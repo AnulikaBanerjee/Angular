10a23009b25fda4764bf960204ff5c6800988257
AnulikaBanerjee <anu.banerjee08@gmail.com>
2018-11-15 21:52:50 +0530

    ToolTipTextEditor

--- SimpleTextEditor/src/app/app.component.ts
+++ SimpleTextEditor/src/app/app.component.ts
@@ -1,31 +1,16 @@
 import { Component } from '@angular/core';
-//import { FormsModule } from '@angular/forms'
-//import { StudentComponent} from './records/records.component'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'AnulikasApp';
-  name:string="avg";
- 
-  industries = [
-    {id: 1, name: "Agriculture"},
-    {id: 2, name: "Manufacturing"},
-    {id: 3, name: "Energy"},
-    {id: 4, name: "Transportation"},
-    {id: 5, name: "Finance"}
-  ];
+  value:string="Edit me";
+  isMiddleDivVisible:boolean=false;
 
-  submitCompany(form){
-    console.log(form.value);
-    alert("The form was submitted");
-    form.reset();
+  private toggleVisibility(){
+    this.isMiddleDivVisible=!this.isMiddleDivVisible;
   }
- // student = new StudentComponent();
-
-  // show(bool){
-  //   this.student.show(bool);
-  // }
 }
